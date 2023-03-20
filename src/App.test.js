cac83c9f130b195f6c907828b64176ef53c5ac29
Serly Nazarian <serly-nazarian@hotmail.com>
2023-03-20 16:10:57 +0400

    wrote more tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import App from "./App";
 import BookingForm from "./components/BookingForm";
@@ -22,10 +22,10 @@ describe("App component", () => {
   });
 });
 
-test('Renders the bookingForm heading', () => {
+test("Renders the bookingForm heading", () => {
   render(<BookingForm />);
   const headingElement = screen.getByText("Booking Form");
-  expect (headingElement).toBeInTheDocument();
+  expect(headingElement).toBeInTheDocument();
 });
 
 test("output is a date object", () => {
@@ -41,4 +41,47 @@ test("updateTimes returns the input date", () => {
   const outputDate = updateTimes(inputDate);
 
   expect(outputDate).toEqual(inputDate);
+});
+
+it("renders the form without errors", () => {
+  render(<BookingForm />);
+});
+
+it("updates the date input value when changed", () => {
+  render(<BookingForm />);
+  const dateInput = screen.getByLabelText("Choose date");
+  fireEvent.change(dateInput, { target: { value: "2023-03-25" } });
+  expect(dateInput).toHaveValue("2023-03-25");
+});
+
+it("updates the number of guests input value when changed", () => {
+  render(<BookingForm />);
+  const guestsInput = screen.getByPlaceholderText("1");
+  fireEvent.change(guestsInput, { target: { value: "4" } });
+  expect(guestsInput).toHaveValue("4");
+});
+
+it("updates the occasion input value wen changed", () => {
+  render(<BookingForm />);
+  const occasionsInput = screen.getByLabelText("Occasion");
+  fireEvent.change(occasionsInput, { target: { value: "Birthday" } });
+  expect(occasionsInput).toHaveValue("Birthday");
+});
+
+it("disables the submit button when the form is invalid", () => {
+  render(<BookingForm />);
+  const submitButton = screen.getByRole("button", {
+    name: "Make your reservation",
+  });
+  expect(submitButton).toBeDisabled();
+
+  const dateInput = screen.getByLabelText("Choose date");
+  const guestsInput = screen.getByLabelText("Number of guests");
+  const occasionsInput = screen.getByLabelText("Occasion");
+
+  fireEvent.change(dateInput, { target: { value: "2023-03-25" } });
+  fireEvent.change(guestsInput, { target: { value: "4" } });
+  fireEvent.change(occasionsInput, { target: { value: "Birthday" } });
+
+  expect(submitButton).toBeEnabled();
 });
--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 
 const BookingForm = (props) => {
   const [date, setDate] = useState("");
-  // const [people, setPeople] = useState(1);
+  const [guests, setGuests] = useState(1);
   const [occasions, setOccasions] = useState(["Birthday", "Anniversary"]);
   // const [preferences, setPreferences] = useState("");
   // const [comments, setComments] = useState("");
+  const [isFormValid, setIsFormValid] = useState(false);
 
   const { availableTimes, submitForm } = props;
 
@@ -18,33 +19,72 @@ const BookingForm = (props) => {
   //   props.updateTimes(date);
   // }
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleInputChange = () => {
+    if (date && time && guests && occasions) {
+      setIsFormValid(true);
+    } else {
+      setIsFormValid(false);
+    }
+  };
+
   return (
-    <form className="reservation-form">
+    <form className="reservation-form" onSubmit={handleSubmit}>
       <h4>Booking Form</h4>
       <label htmlFor="res-date">Choose date</label>
-      <input value={date} onChange={setDate} type="date" id="res-date" />
-      {/* <label htmlFor="res-time">Choose time</label> */}
-      {/* <select id="res-time">
+      <input
+        pattern="\d{4}-\d{2}-\d{2}"
+        value={date}
+        onChange={(e) => {
+          setDate(e.target.value);
+          handleInputChange();
+        }}
+        type="date"
+        id="res-date"
+        required
+      />
+      <label htmlFor="res-time">Choose time</label>
+      <select id="res-time">
         {availableTimes.map((time) => (
           <option key={time} value={time}>
             {time}
           </option>
         ))}
-      </select> */}
+      </select>
       <label htmlFor="guests">Number of guests</label>
-      <input type="number" placeholder="1" min="1" max="10" id="guests" />
-      {/* <label htmlFor="occasion">Occasion</label> */}
-      {/* <select id="occasion" value={occasions} onChange={setOccasions}>
+      <input
+        type="number"
+        placeholder="1"
+        min="1"
+        max="10"
+        id="guests"
+        value={guests}
+        onChange={e => {setGuests(e.target.value); handleInputChange();}}
+        required
+      />
+      <label htmlFor="occasion">Occasion</label>
+      <select
+        id="occasion"
+        value={occasions}
+        onChange={(e) => {
+          setOccasions(e.target.value);
+          handleInputChange();
+        }}
+      >
         {occasions.map((occasion) => (
           <option key={occasion} value={occasion}>
             {occasion}
           </option>
         ))}
-      </select> */}
+      </select>
       <input
         type="submit"
         value="Make your reservation"
         onSubmit={submitForm}
+        disabled={!isFormValid}
       />
     </form>
   );
